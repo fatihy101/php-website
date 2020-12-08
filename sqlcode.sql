45565c7cdf65493b48ab46fb3a39dedc3fb29178
Fatih <fatihy@yahoo.com>
2020-12-09 00:07:34 +0300

    bugfix

--- sqlcode.sql
+++ sqlcode.sql
@@ -5,7 +5,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 
 DROP TABLE IF EXISTS `AboutUs`;
 CREATE TABLE `AboutUs` (
