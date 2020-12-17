2ecf4cf9fac1ae313cf7af19f4e9865b5473945c
afakgun <afakgun17@gmail.com>
2020-12-17 16:36:25 +0300

    Form yönlendirme butonları telefon için resize

--- regular-template/galeri.php
+++ regular-template/galeri.php
@@ -31,29 +31,29 @@ $results = $conn->query($SQL);
        <?php }  ?>
 
         </div>
-    </div>
+    
 
 
-    <!-- .related post block --><br> <br>
-    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-        <div class="next-prev-post">
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <div class="prev-post">
-                        <h3 class="prev-link-title"><a href="#" class="title">Hayellerinizdeki düğün için</a></h3>
-                        <p><a href="index.php" class="prev-link btn-link">Hemen Başvurun!</a></p>
+        <!-- .related post block --><br> <br>
+        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+            <div class="next-prev-post">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="prev-post">
+                            <h3 class="prev-link-title"><a href="#" class="title">Hayellerinizdeki düğün için</a></h3>
+                            <p><a href="index.php" class="prev-link btn-link">Hemen Başvurun!</a></p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <div class="next-post">
-                        <h3 class="next-link-title"><a href="#" class="title">Merak ettiğiniz bir konu için</a></h3>
-                        <p><a href="iletisim.php" class="next-link btn-link">Bizimle iletişime geçin!</a></p>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="next-post">
+                            <h3 class="next-link-title"><a href="#" class="title">Merak ettiğiniz bir konu için</a></h3>
+                            <p><a href="iletisim.php" class="next-link btn-link">Bizimle iletişime geçin!</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <?php require 'footer-default.php';?>
     <script src="js/lightbox-plus-jquery.min.js"></script>
 
