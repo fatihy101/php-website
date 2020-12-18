c05d1762ba94fe368ce4b91746632e0057e17c27
Fatih <fatihy@yahoo.com>
2020-12-18 20:53:04 +0300

    Images deleted.

--- regular-template/404error-page.php
+++ regular-template/404error-page.php
@@ -9,14 +9,7 @@
    <?php require 'header-default.php';?>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-                <div class="page-breadcrumb">
-                    <ol class="breadcrumb">
-                        <li><a href="index.php">Home</a></li>
-                        <li class="active">Error page</li>
-                    </ol>
-                </div>
-            </div>
+            
             <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
                 <div class="page-section">
                     <h1 class="page-title">Error page</h1></div>
